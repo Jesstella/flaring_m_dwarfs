4012fe95e4125dfbff90769333f2216de786d8e8
Jesstella <jessicaschonhut@gmail.com>
2019-09-17 15:26:31 -1000

    Update trilegal_cut_to_mathur.py

--- trilegal_cut_to_mathur.py
+++ trilegal_cut_to_mathur.py
@@ -10,15 +10,15 @@ import csv
 import os
 
 #Choose paths
-mathur_path = '/Users/Jess/699-2/mathur_2017/'
-fig_path = '/Users/Jess/699-2/final_github_code/'
-trilegal_path = '/Users/Jess/699-2/trilegal_data/'
+mathur_path = '/Users/jessicaschonhut/flares_paper/mathur_data/'
+fig_path = '/Users/jessicaschonhut/flares_paper/'
+trilegal_path = '/Users/jessicaschonhut/flares_paper/trilegal_data/'
 
 #Pull Mathur data for temp, logg and mag
-mathur_teff = pd.read_csv(mathur_path + 'mathur_2017_edited.txt', delimiter='|')['Teff'] #Teff
+mathur_teff = pd.read_csv(mathur_path + 'mathur_sample_complete.txt', delimiter=',')['temp'] #Teff
 mathur_teff = np.log10(mathur_teff) 
-mathur_logg = pd.read_csv(mathur_path + 'mathur_2017_edited.txt', delimiter='|')['log(g)'] #Logg
-mathur_mag = pd.read_csv(mathur_path + 'mathur_master.txt', delimiter=',')['kepmag'] #Mag
+mathur_logg = pd.read_csv(mathur_path + 'mathur_sample_complete.txt', delimiter=',')['logg'] #Logg
+mathur_mag = pd.read_csv(mathur_path + 'mathur_sample_complete.txt', delimiter=',')['kepmag'] #Mag
 
 #CHECK
 print('The number of stars in the original Mathur file is: ' + str(len(mathur_teff)))
@@ -28,9 +28,9 @@ print('The number of stars in the file of magnitudes is: ' + str(len(mathur_mag)
 mathur_sample = np.vstack([mathur_teff, mathur_logg, mathur_mag])
 
 #Bins for the three parameters 
-num_bins=25 #Bins for Teff 
-num_bins_logg=26 #Bins for logg
-num_bins_mag=27 #Bins for mag
+num_bins=20 #Bins for Teff 
+num_bins_logg=21 #Bins for logg
+num_bins_mag=22 #Bins for mag
 
 #Histogram for teff
 #Range set is for temperature
@@ -74,47 +74,46 @@ N_temp_mag = np.sum(N_sum_mathur, axis=1) #temperature vs. mag
 N_mag_logg = np.sum(N_sum_mathur, axis=0) #mag vs. logg
 
 #Plotting 2D density plots
-plt.figure(figsize=(50, 25)) 
-plt.subplot(131) 
+plt.figure(figsize=(60, 45)) 
+plt.subplot(311) 
 plt.imshow(N_temp_logg) 
-plt.yticks(range(num_bins-1), bin_centers_teff_ticks)
-plt.xticks(range(num_bins_logg-1), bin_centers_logg_ticks)
 plt.ylabel('Effective Temperature [K]', fontsize=30) 
 plt.xlabel('log(g)', fontsize=30) 
-plt.xticks(fontsize=10) 
-plt.yticks(fontsize=10) 
+plt.xticks(fontsize=30) 
+plt.ylim(12.5, 0)
+plt.xlim(2.5, 20)
+plt.yticks(fontsize=30) 
 plt.title(r'Number Density for log(g) and' +  '\n' + '$T_{eff}$ for the Mathur Sample', fontsize=30) 
 
-plt.subplot(132) 
+plt.subplot(312) 
 plt.imshow(N_temp_mag)
-plt.yticks(range(num_bins-1), bin_centers_teff_ticks)
-plt.xticks(range(num_bins_mag-1), bin_centers_mag_ticks)
 plt.ylabel('Effective Temperature [K]', fontsize=30)
 plt.xlabel('Kepler Magnitude', fontsize=30)
-plt.xticks(fontsize=10) 
-plt.yticks(fontsize=10) 
+plt.xticks(fontsize=30) 
+plt.ylim(12.5, 0)
+plt.xlim(2.5, 17.5)
+plt.yticks(fontsize=30) 
 plt.title(r'Number Density for Kepler Magnitude and' +  '\n' + '$T_{eff}$ for the Mathur Sample', fontsize=30) 
 
-plt.subplot(133)
+plt.subplot(313)
 plt.imshow(N_mag_logg)
-plt.yticks(range(num_bins_logg-1), bin_centers_logg_ticks)
-plt.xticks(range(num_bins_mag-1), bin_centers_mag_ticks)
 plt.xlabel('Kepler Magnitude', fontsize=30)
 plt.ylabel('log(g)', fontsize=30)
-plt.xticks(fontsize=10) 
-plt.yticks(fontsize=10) 
+plt.xticks(fontsize=30) 
+plt.ylim(20, 7.5)
+plt.xlim(7.5, 20)
+plt.yticks(fontsize=30) 
 plt.title(r'Number Density for Kepler Magnitude and' +  '\n' + 'log(g) for the Mathur Sample', fontsize=30) 
-plt.colorbar() 
 
 #Save and show full figure
 plt.savefig(fig_path + 'ss18_mathur_binned_stars.png')
 
 #Pull TRILEGAL values
 print('running') #Test print for long run
-trilegal_teff = pd.read_csv(trilegal_path + 'trilegal_master_with_index.csv', delimiter=',')['logTe'] #Teff
-trilegal_logg = pd.read_csv(trilegal_path + 'trilegal_master_with_index.csv', delimiter=',')['logg'] #Logg
-trilegal_mag = pd.read_csv(trilegal_path + 'trilegal_master_with_index.csv', delimiter=',')['Kepler'] #Magnitude
-trilegal_index = pd.read_csv(trilegal_path + 'trilegal_master_with_index.csv', delimiter=',')['Index'] #Index
+trilegal_teff = pd.read_csv(trilegal_path + 'trilegal_master_sample.txt', delimiter=',')['logTe'] #Teff
+trilegal_logg = pd.read_csv(trilegal_path + 'trilegal_master_sample.txt', delimiter=',')['logg'] #Logg
+trilegal_mag = pd.read_csv(trilegal_path + 'trilegal_master_sample.txt', delimiter=',')['Kepler'] #Magnitude
+trilegal_index = pd.read_csv(trilegal_path + 'trilegal_master_sample.txt', delimiter=',')['index'] #Index
 
 #Converting TRILGEAL data from pd series to arrays
 trilegal_teff = np.array(trilegal_teff) #Teff
@@ -124,9 +123,9 @@ trilegal_index = np.array(trilegal_index)
 
 #This is the same process as run for the Mathur sample above.
 print('running') #Test print for long run
-num_bins=25
-num_bins_logg=26
-num_bins_mag=27
+num_bins=20
+num_bins_logg=21
+num_bins_mag=22
 
 hist_teff, bin_edges_teff = np.histogram(trilegal_teff, bins=num_bins, range=(3.35, 4.5))
 bin_centers_teff = ((bin_edges_teff[:-1] + bin_edges_teff[1:]) / 2)
@@ -163,37 +162,35 @@ N_temp_logg = np.sum(N_sum, axis=2)
 N_temp_mag = np.sum(N_sum, axis=1)
 N_logg_mag = np.sum(N_sum, axis=0)
 
-plt.figure(figsize=(50, 25))
-plt.subplot(131)
+plt.figure(figsize=(60, 45))
+plt.subplot(311)
 plt.imshow(N_temp_logg)
-plt.yticks(range(num_bins-1), (bin_centers_teff_ticks))
-plt.xticks(range(num_bins_logg-1), (bin_centers_logg_ticks))
 plt.ylabel('Effective Temperature [K]', fontsize=30)
 plt.xlabel('log(g)', fontsize=30)
-plt.xticks(fontsize=10) 
-plt.yticks(fontsize=10) 
+plt.xticks(fontsize=30) 
+plt.yticks(fontsize=30) 
+plt.ylim(13, 1)
 plt.title(r'Number Density for log(g) and' +  '\n' + '$T_{eff}$ for the TRILEGAL Sample', fontsize=30) 
 
-plt.subplot(132)
+plt.subplot(312)
 plt.imshow(N_temp_mag)
-plt.yticks(range(num_bins-1), (bin_centers_teff_ticks))
-plt.xticks(range(num_bins_mag-1), (bin_centers_mag_ticks))
 plt.ylabel('Effective Temperature [K]', fontsize=30)
 plt.xlabel('Kepler Magnitude', fontsize=30)
-plt.xticks(fontsize=10) 
-plt.yticks(fontsize=10)
+plt.xticks(fontsize=30) 
+plt.yticks(fontsize=30)
+plt.xlim(4,21)
+plt.ylim(10, 1)
 plt.title(r'Number Density for Kepler Magnitude and' +  '\n' + '$T_{eff}$ for the TRILEGAL Sample', fontsize=30) 
 
-plt.subplot(133)
+plt.subplot(313)
 plt.imshow(N_logg_mag)
-plt.yticks(range(num_bins_logg-1), (bin_centers_logg_ticks))
-plt.xticks(range(num_bins_mag-1), (bin_centers_mag_ticks))
 plt.xlabel('Kepler Magnitude', fontsize=30)
 plt.ylabel('log(g)', fontsize=30)
-plt.xticks(fontsize=10) 
-plt.yticks(fontsize=10)
+plt.xticks(fontsize=30) 
+plt.yticks(fontsize=30)
+plt.ylim(20, 7.5)
+plt.xlim(7.5, 21)
 plt.title(r'Number Density for Kepler Magnitude and' +  '\n' + 'log(g) for the TRILEGAL Sample', fontsize=30) 
-plt.colorbar()
 
 plt.savefig(fig_path + 'ss18_trilegal_binned_stars.png')
 
@@ -241,22 +238,20 @@ for sublist in trilegal_index_new:
         list_trilegal_index.append(item)
 
 #Collect data for the new file from TRILEGAL, using the new index list
-df = pd.read_csv(trilegal_path +'trilegal_master_with_index_compare.csv')
+df = pd.read_csv(trilegal_path +'trilegal_master_sample.txt')
 with open(fig_path + 'final_sample_initial.csv', 'a', newline='') as f_outputcsv: 
     csv_writer = csv.writer(f_outputcsv) 
-    row_head = ['Index','a','Gc','logAge','[M/H]','m_ini','logL','logTe','logg','m-M0','Av','m2/m1','mbol','Kepler','g','r','i','z','DDO51_finf','Mact','z']
+    row_head = ['index','Gc','logAge','[M/H]','m_ini','logL','logTe','logg','m-M0','Av','m2/m1','mbol','Kepler','g','r','i','z','DDO51_finf','J','H','Ks','Mact']
     csv_writer.writerow(row_head)
     for i in list_trilegal_index:  
         row = df.iloc[int(i)] 
         csv_writer.writerow(row) 
         
 df = pd.read_csv(fig_path + 'final_sample_initial.csv')
-df.drop('a', axis = 1, inplace = True)
-df.drop('z', axis = 1, inplace = True)
 df.to_csv(fig_path + 'final_sample.csv', index = False)    
 os.remove(fig_path + 'final_sample_initial.csv')
 
-list_trilegal_temps = pd.read_csv(fig_path + 'final_sample.csv')['logTe']
+list_trilegal_temps = pd.read_csv(fig_path + 'final_sample.csv')['logTe'] 
 list_trilegal_logg = pd.read_csv(fig_path + 'final_sample.csv')['logg']
 list_trilegal_mags = pd.read_csv(fig_path + 'final_sample.csv')['Kepler']
 
@@ -282,7 +277,6 @@ plt.hist(mathur_logg, histtype='step', linestyle='--', color='#33a02c', linewidt
 plt.hist(list_trilegal_logg, histtype='step', linestyle='-', color='#1f78b4', linewidth=3)
 plt.yscale('log')
 plt.xlabel('Log(g)', fontsize=20)
-#plt.ylabel('N', fontsize=20)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 plt.title('Histogram to Samples for log(g)', fontsize=20)
@@ -290,16 +284,14 @@ plt.title('Histogram to Samples for log(g)', fontsize=20)
 #Histogram for Kepler magnitude - see above 
 plt.subplot(133)
 plt.hist(trilegal_mag, histtype='step', label='Trilegal', linestyle=':', color='#e31a1c', linewidth=3) #Set sample label 
-#Set sample label below for Mathur
 plt.hist(mathur_mag, range=(min(mathur_mag), max(mathur_mag)), histtype='step', label='Mathur', linestyle='--', color='#33a02c', linewidth=3) 
 plt.hist(list_trilegal_mags, histtype='step', label='New Sample', linestyle='-', color='#1f78b4', linewidth=3) #Set sample label 
 plt.yscale('log')
 plt.xlabel('Kepler Magnitude', fontsize=20)
-#plt.ylabel('N', fontsize=20)
 plt.xticks(fontsize=20)
 plt.yticks(fontsize=20)
 plt.title('Histogram of Samples for Kepler Magnitude', fontsize=20)
-plt.legend(fontsize=20) #Plot legend
+plt.legend(fontsize=20) 
 
 #Save and show figure 
 plt.savefig(fig_path + 'final_sample_comparison.png')
